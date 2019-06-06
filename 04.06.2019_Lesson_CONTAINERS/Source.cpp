683bac15053104fbb754edb59dc06eaf3efd90fe
айкеновад <@205_8>
2019-06-06 12:01:21 +0600

    -

--- 04.06.2019_Lesson_CONTAINERS/Source.cpp
+++ 04.06.2019_Lesson_CONTAINERS/Source.cpp
@@ -15,54 +15,54 @@ bool asc(train t1, train t2) {
 
 int main() {
 
-	//vector<int> v1(10);//10 els, emptys will be nulls
-	//v1.push_back(4);
-	//v1.push_back(8);
-	//v1.pop_back();
-	//v1.insert(v1.begin()+1, 7);//begin() return ptr to the beginning of the vector
-	//v1.insert(v1.end() - 3, 10);//minus 3 pos from the end
-	//v1.insert(v1.begin(), 500);
-	//vector<int>::iterator it;
+	vector<int> v1(10);//10 els, emptys will be nulls
+	v1.push_back(4);
+	v1.push_back(8);
+	v1.pop_back();
+	v1.insert(v1.begin()+1, 7);//begin() return ptr to the beginning of the vector
+	v1.insert(v1.end() - 3, 10);//minus 3 pos from the end
+	v1.insert(v1.begin(), 500);
+	vector<int>::iterator it;
 
-	////#include<algorithm>
-	//replace(v1.begin() + 5, v1.begin() + 8, 0, 1000); //replace 0 to 1000 on positions from 5 till 8
-	//*find(v1.begin(), v1.end(), 10) = 2000; //find - returns ptr to el, *find - its value
+	//#include<algorithm>
+	replace(v1.begin() + 5, v1.begin() + 8, 0, 1000); //replace 0 to 1000 on positions from 5 till 8
+	*find(v1.begin(), v1.end(), 10) = 2000; //find - returns ptr to el, *find - its value
 
-	////vector<int>::iterator it = find(v1.begin(), v1.end(), 300);
-	////if (it != v1.end())//если нашел элемент=300, т.е если не дошел до конца
-	////	*it = 3000;
+	//vector<int>::iterator it = find(v1.begin(), v1.end(), 300);
+	//if (it != v1.end())//если нашел элемент=300, т.е если не дошел до конца
+	//	*it = 3000;
 
-	////to find and replace not only one but all such elements we use cycle
-	//it = v1.begin();
-	//while (it != v1.end()) {
-	//	it = find(it++, v1.end(), 0);
-	//	if (it != v1.end())
-	//		*it = 4000;
-	//}
+	//to find and replace not only one but all such elements we use cycle
+	it = v1.begin();
+	while (it != v1.end()) {
+		it = find(it++, v1.end(), 0);
+		if (it != v1.end())
+			*it = 4000;
+	}
 
-	//int number;
-	//cin >> number;
+	int number;
+	cin >> number;
 
-	//v1.erase(v1.begin());
-	//v1.erase(v1.begin(), v1.begin()+5);//2 arguments
-	//
-	//for_each(v1.begin(), v1.end(), [number](int a) {
-	//	if(a==number)
-	//	cout << a << endl; });//с начала до конца примени лямбда функцию// int a - псевдоним для каждого элемента, соответсвующего типа
-	//cout << endl;
+	v1.erase(v1.begin());
+	v1.erase(v1.begin(), v1.begin()+5);//2 arguments
+	
+	for_each(v1.begin(), v1.end(), [number](int a) {
+		if(a==number)
+		cout << a << endl; });//с начала до конца примени лямбда функцию// int a - псевдоним для каждого элемента, соответсвующего типа
+	cout << endl;
 
-	//v1.erase(remove_if(v1.begin(), v1.end(),
-	//	//лямбда функция [ {condition} ] определяет по какому условию будет удалять элементы
-	//	[number](int a) {
-	//	return (number == a);
-	//}
-	//)/*first arg of erase()*/, v1.end());/*second arg of erase()*/
+	v1.erase(remove_if(v1.begin(), v1.end(),
+		//лямбда функция [ {condition} ] определяет по какому условию будет удалять элементы
+		[number](int a) {
+		return (number == a);
+	}
+	)/*first arg of erase()*/, v1.end());/*second arg of erase()*/
 
-	//cout << "The vector without " << number << endl;
-	//for_each(v1.begin(), v1.end(), [](int a) { cout << a << endl; });//print all, применяет лямбда функцию к каждому элементу вектора
+	cout << "The vector without " << number << endl;
+	for_each(v1.begin(), v1.end(), [](int a) { cout << a << endl; });//print all, применяет лямбда функцию к каждому элементу вектора
 
-	//for (it = v1.begin(); it != v1.end(); it++)
-	//	cout << *it << endl;// * для вывода значения под итератором (указателем) it
+	for (it = v1.begin(); it != v1.end(); it++)
+		cout << *it << endl;// * для вывода значения под итератором (указателем) it
 
 	//library STL has ready classes for:
 	//Containers(vector, list, set, map, deque; ADT abstract data types - stack, queue),
@@ -112,60 +112,60 @@ int main() {
 
 
 
-	////with vector
+	//with vector
 
-	//ifstream in_file("in.txt");
-	//vector<train> ts;
-	//int n;
-	//string str1, str2;
-	//string feature;
+	ifstream in_file("in.txt");
+	vector<train> tss;
+	int n;
+	string str1, str2;
+	string feature;
 
-	//if (!in_file)
-	//	cerr << "file open error" << endl;
-	//else {
-	//	while (in_file >> n >> str1 >> str2) {//пока идет считывание
-	//		ts.push_back(train(n, str2, str1));
-	//	}
-	//}
+	if (!in_file)
+		cerr << "file open error" << endl;
+	else {
+		while (in_file >> n >> str1 >> str2) {//пока идет считывание
+			tss.push_back(train(n, str2, str1));
+		}
+	}
 
-	////sort by number
-	//sort(ts.begin(), ts.end(), asc);//our own predicator asc
+	//sort by number
+	sort(tss.begin(), tss.end(), asc);//our own predicator asc
 
-	//cin >> feature;
-	////outer method SortBy
-	//sort(ts.begin(), ts.end(), SortBy(feature));//functor SortBy, класс при вызове круглых скобок становится функтором
-	//for_each(ts.begin(), ts.end(), [](train t){cout << t << endl;});
-
-
-	////for_each(ts.begin(), ts.end(), [](train t) {cout << t << endl; });
-
-	////or with filter!
-	//
-	//cout << "Enter \"number\" to search by number" << endl;
-	//cout << "Enter \"dep_time\" to search by departure time" << endl;
-	//cout << "Enter \"destination\" to search by destination" << endl;
-	//cin >> feature;
-	//train t;
-
-	//
-	//if (feature == "number") {
-	//	cin >> n;
-	//	t.getNumber() = n;
-	//}
-	//else if (feature == "dep_time") {
-	//	cin >> str1;
-	//	t.getDepartureTime() = str1;
-	//}
-	//else if (feature == "destination") {
-	//	cin >> str2;
-	//	t.getDestination() = str2;
-	//}
+	cin >> feature;
+	//outer method SortBy
+	sort(tss.begin(), tss.end(), SortBy(feature));//functor SortBy, класс при вызове круглых скобок становится функтором
+	for_each(tss.begin(), tss.end(), [](train tr){cout << tr << endl;});
 
 
-	////filter is a FUNCTOR
-	//for_each(ts.begin(), ts.end(), filter(feature, t));//here using operator()!!!
-	////filter compares this train t to vector's objects and implements own condition
-	////predicator is a functor that returns bool. Usually functors return void
+	//for_each(ts.begin(), ts.end(), [](train t) {cout << t << endl; });
+
+	//or with filter!
+	
+	cout << "Enter \"number\" to search by number" << endl;
+	cout << "Enter \"dep_time\" to search by departure time" << endl;
+	cout << "Enter \"destination\" to search by destination" << endl;
+	cin >> feature;
+	train tr;
+
+	
+	if (feature == "number") {
+		cin >> n;
+		tr.getNumber() = n;
+	}
+	else if (feature == "dep_time") {
+		cin >> str1;
+		tr.getDepartureTime() = str1;
+	}
+	else if (feature == "destination") {
+		cin >> str2;
+		tr.getDestination() = str2;
+	}
+
+
+	//filter is a FUNCTOR
+	for_each(ts.begin(), ts.end(), filter(feature, tr));//here using operator()!!!
+	//filter compares this train t to vector's objects and implements own condition
+	//predicator is a functor that returns bool. Usually functors return void
 
 
 	system("pause");
